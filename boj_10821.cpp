d59460a8ba0b0515e51c9eaddb1c53e6739bad47
riullife <30718646+riullife@users.noreply.github.com>
2018-05-21 15:58:42 +0900

    정수의 개수
    정수의 개수 = ','개수 + 1
    strtok 써서 풀 수 있을 것 같은데 이게 더 쉬움

---
+++ boj_10821.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+  string s;
+  int cnt=0;
+
+  cin>> s;
+
+  for(int i=0;i<s.size();i++)
+  {
+    if (s[i]==',')
+    cnt++;
+  }
+  cout << cnt+1 << endl;
+}
